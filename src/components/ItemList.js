14584f195ebc03c47b238f6de5f7e25d3e8d54e5
alexoid <alex_zam@hotmail.es>
2021-07-27 01:44:21 -0500

    add item containers

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,8 +5,25 @@ function ItemList() {
     return (
 
     <div className="itemContainer">
-        <h1 className="title">Select Builder Schedule - 2019 Fall Cohort</h1>
-        <div class="boxContainer">  
+        <h1 className="title fontStyle">Select Builder Schedule - 2019 Fall Cohort</h1>
+        <div className="boxContainer">
+            <div>
+                <div>
+                    <h3 className="inovation fontStyle">Social Innovation</h3>
+                </div>
+               
+                <div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                    <div className="item">
+
+                    </div>
+                </div>
+            </div>  
 
         </div>
     </div>
